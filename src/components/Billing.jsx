d571e6bbba8687f0c9c1f9f636f79defb37f8cf6
kevinkupervaser <kevinkupervaser@gmail.com>
2022-08-18 16:00:01 -0300

    initial

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -11,8 +11,9 @@ const Billing = () => (
       />
 
       {/* Gradient start */}
-      <div className='absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient' />
-      <div className='absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient' />
+      {/* <div className='absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient' /> */}
+      {/* <div className='absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient' /> */}
+      <div className='absolute z-[0] w-[60%] h-[60%] -right-[40%] rounded-full blue__gradient bottom-40' />
       {/* Gradient end */}
     </div>
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -49,9 +49,10 @@ const Hero = () => {
         />
 
         {/* gradient start */}
-        <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient' />
-        <div className='absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40' />
-        <div className='absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient' />
+        {/* <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient' /> */}
+        {/* <div className='absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40' />
+        <div className='absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient' /> */}
+        <div className='absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-40' />
         {/* gradient end */}
       </div>
 
